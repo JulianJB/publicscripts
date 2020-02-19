c67026f47b9a1b47c2121bc7ec3e8185d2746145
JulianJB <61026988+JulianJB@users.noreply.github.com>
2020-02-19 02:39:21 +0000

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,7 +1,7 @@
 // This is the uncompressed code used in the bookmarklet
 // This is just for demonstration, the real code is in the html
 
-(function () {
+;(function () {
 
     alert('Ay mama que me den mas tequila');
 
