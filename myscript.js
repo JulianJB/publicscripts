e20e253c36f69ee59afc022eec42ca498e9950b0
JulianJB <61026988+JulianJB@users.noreply.github.com>
2020-02-19 02:08:59 +0000

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,8 @@
+// This is the uncompressed code used in the bookmarklet
+// This is just for demonstration, the real code is in the html
+
+(function () {
+
+    //alert('Ay mama que me den mas tequila');
+
+})(); // Alias jQuery to $ in our wrapper function.
